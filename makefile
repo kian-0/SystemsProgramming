f6cd376328358aa476b8b898ebee9c88e7c9e849
kian-0 <oliverkian01@gmail.com>
2024-09-30 01:46:40 -0400

    On branch 093024  Your branch is up to date with 'origin/093024'.
     Changes to be committed:
    	modified:   InsertSymbol.c
    	modified:   headers.h
    	modified:   makefile
    	modified:   p1.c
    
     Untracked files:
    	.vscode/
    	InsertSymbol.o
    	IsInSymbolTable.o
    	PrintSymbolTable.o
    	a.out
    	p1.o
    	project1

--- makefile
+++ makefile
@@ -1,5 +1,5 @@
-project1: p1.o PrintSymbolTable.o
-		gcc -o project1 p1.o PrintSymbolTable.o
+project1: p1.o PrintSymbolTable.o InsertSymbol.o IsInSymbolTable.o
+		gcc -o project1 p1.o PrintSymbolTable.o InsertSymbol.o IsInSymbolTable.o
 
 p1.o: p1.c headers.h
 		gcc -c -g -Wall p1.c
@@ -7,8 +7,8 @@ p1.o: p1.c headers.h
 PrintSymbolTable.o: PrintSymbolTable.c headers.h
 		gcc -c -g -Wall PrintSymbolTable.c
 
+InsertSymbol.o: InsertSymbol.c headers.h
+		gcc -c -g -Wall InsertSymbol.c
+
 IsInSymbolTable.o: IsInSymbolTable.c headers.h
 		gcc -c -g -Wall IsInSymbolTable.c
-
-InsertSymbolTable.o: InsertSymbolTable.c headers.c
-		gcc -c -g -Wall InsertSymbolTable.c
--- p1.c
+++ p1.c
@@ -6,39 +6,43 @@
  */
 #include "headers.h"
 
-int p1(int argc, char *argv[]) {
+int main(int argc, char *argv[]) {
     //Starts up file
+    FILE *file;
     char line[1024];
     char *token;
 
     if (argc != 2) {
         printf("Usage: %s <filename>. - where file name is a SIC Assembly file\n", argv[0]);
-        return;
+        return 0;
     }
-    FILE *file = fopen(argv[1], "r"); //Opens file
+
+    file = fopen(argv[1], "r"); //Opens file
 
     if (file == NULL) {
         printf("Error opening file %s\n", argv[1]);
-        return;
+        return 0;
     }
 
     while (fgets(line, 1024, file) != NULL) {
 
         printf("Reading: %s", line);
 
+        if (line[0] == 35)
+        {
+            printf("Comment line - Skipping\n");
+            continue;
+        } // end comment if
+        
         if(strlen(line) <= 2) {
             printf("Error: Empty line\n");
             fclose(file);
             return 0;
         }//end strlen if
 
-        if (line[0] == 35) {
-            printf("Comment line - Skipping\n");
-            continue;
-        }//end comment if
-
         while (token - strtok(line, " ")) {
             printf("\tToken: %s\n", token);
+            token = strtok(NULL, " \t\n");
         }
 
     }//end fgets while loop

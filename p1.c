550d99954da0e617e63ee11d1cdb9f6f3ceec290
kian-0 <oliverkian01@gmail.com>
2024-10-01 19:48:18 -0400

    modified:   p1.c

--- p1.c
+++ p1.c
@@ -10,8 +10,10 @@ int main(int argc, char *argv[]) {
     //Starts up file
     FILE *file;
     char line[1024];
-    char *token;
-
+    char symbol[32];
+    char opcode[32];
+    char operand[32];
+    
     if (argc != 2) {
         printf("Usage: %s <filename>. - where file name is a SIC Assembly file\n", argv[0]);
         return 0;
@@ -28,22 +30,24 @@ int main(int argc, char *argv[]) {
 
         printf("Reading: %s", line);
 
+        //Checks beginning of line for # (a comment)
         if (line[0] == 35)
         {
             printf("Comment line - Skipping\n");
             continue;
         } // end comment if
         
+        //Checks to see if it is an empty line and ends the program
         if(strlen(line) <= 2) {
             printf("Error: Empty line\n");
             fclose(file);
             return 0;
         }//end strlen if
 
-        while (token - strtok(line, " ")) {
-            printf("\tToken: %s\n", token);
-            token = strtok(NULL, " \t\n");
-        }
+        //Splits the line into core parts
+        sscanf(line, "%s %s %s", symbol, opcode, operand);
+
+        printf("Symbol:%s\n Opcode:%s\n Operand:%s\n", symbol, opcode, operand);
 
     }//end fgets while loop
 

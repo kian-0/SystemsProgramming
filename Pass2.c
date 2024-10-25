b2a5382d926e0e5f79ba7ab92b9d21f1ab0d9aab
kian-0 <oliverkian01@gmail.com>
2024-10-25 00:48:29 -0400

    new file:   Pass2.c

---
+++ Pass2.c
@@ -0,0 +1,31 @@
+#include "headers.h"
+
+void Pass2(){
+    //ADD
+    //AND
+    //COMP
+    //DIV
+    //J
+    //JEQ
+    //JGT
+    //JGT
+    //JLT
+    //JSUB
+    //LDA
+    //LDCH
+    //LDL
+    //LDX
+    //MUL
+    //OR
+    //RD
+    //RSUB
+    //STA
+    //STCH
+    //STL
+    //STSW
+    //STX
+    //SUB
+    //TD?
+    //TIX
+    //WD?
+}
